60b632762df32c43b9dac55ae6fb97867441c651
martinaskolin <martinaskolin99@gmail.com>
2025-03-19 10:28:46 +0100

    asdadsasd

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ def forward_request():
     incoming_data = request.get_json()
 
     # Forward data to localhost:9092
-    try:
-        response = requests.post('http://localhost:9092', json=incoming_data)
+    while True:
+        try:
+            response = requests.post('http://localhost:9092', json=incoming_data)
 
-        # Return response from localhost:9092 to the client
-        return jsonify(response.json()), response.status_code
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": f"Failed to forward request: {str(e)}"}), 500
+            # Return response from localhost:9092 to the client
+            if response.status_code == 200:
+                return jsonify(response.json()), response.status_code
+            
+        except requests.exceptions.RequestException as e:
+            print("Failed to POST")
+            #return jsonify({"error": f"Failed to forward request: {str(e)}"}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9091)
